3690645c82a606ff5f895e701eabd8f9e43b986b
omrsfylmz <omrsfylmz@gmail.com>
2022-11-14 00:43:41 +0300

    Update search `page`

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Button from "./components/Button";
 import Search from "./pages/Search";
 
 function App() {
   return (
     <div>
       <Search />
-      <Button />
     </div>
   );
 }
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,9 +1,23 @@
+import Button from "../../components/Button";
+import Header from "../../components/Header";
 import Search from "../../components/SearchBar";
+import googleLogo from "../../assets/google-logo.png";
+import "./styles.scss";
 
 const index = () => {
   return (
-    <div>
-      <Search />
+    <div className="search-page-container">
+      <Header />
+      <div className="search-page">
+        <img src={googleLogo} alt="google-logo" className="google-logo" />
+        <div className="search-page-inner">
+          <Search />
+          <div className="search-page-button-container">
+            <Button />
+            <Button />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
