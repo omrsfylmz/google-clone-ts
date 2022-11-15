60eb0a8baad452bd95747d888ec9bacf08b0a82f
omrsfylmz <omrsfylmz@gmail.com>
2022-11-15 16:35:58 +0300

    Add categories

---
+++ src/components/Categories/index.tsx
@@ -0,0 +1,20 @@
+import "./styles.scss";
+
+const index = () => {
+  return (
+    <div className="categories-container">
+      <div className="categories-inner">
+        <div className="category-item">All</div>
+        <div className="category-item">Images</div>
+        <div className="category-item">News</div>
+        <div className="category-item">Videos</div>
+        <div className="category-item">Maps</div>
+        <div className="category-item">More</div>
+        <div className="category-item">Settings</div>
+        <div className="category-item">Tools</div>
+      </div>
+    </div>
+  );
+};
+
+export default index;
