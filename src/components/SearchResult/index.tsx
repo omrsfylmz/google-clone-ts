5d27d2f2d0aad9614b5588535ebacaed276ac097
omrsfylmz <omrsfylmz@gmail.com>
2022-11-15 16:36:19 +0300

    Add `search-result` component

---
+++ src/components/SearchResult/index.tsx
@@ -0,0 +1,29 @@
+import "./styles.scss";
+
+const index = () => {
+  return (
+    <div className="search-result-container">
+      <div className="search-result-container-header-features">
+        <div className="display-link">
+          <a href="xxxx">https://en.wikipedia.org</a>
+        </div>
+        <a href="">
+          <div className="title">Car - Wikipedia</div>
+        </a>
+      </div>
+      <div className="description">
+        A car or automobile is a motor vehicle with wheels. Most definitions of
+        cars say that they run primarily on roads, seat one to eight people, ...
+        A car or automobile is a motor vehicle with wheels. Most definitions of
+        cars say that they run primarily on roads, seat one to eight people, ...
+        A car or automobile is a motor vehicle with wheels. Most definitions of
+        cars say that they run primarily on roads, seat one to eight people, ...
+      </div>
+      <div className="thumbnail">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/401_Gridlock.jpg/300px-401_Gridlock.jpg" />
+      </div>
+    </div>
+  );
+};
+
+export default index;
